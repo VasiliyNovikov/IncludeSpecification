94795cbf99d6c62c88df78b428dff408fa57983a
Vasiliy Novikov <vasya_n@hotmail.com>
2014-11-12 01:31:44 +0200

    Test database util fixed

---
+++ Sources/IncludeSpec.Test.Utility/TestDatabase.cs
@@ -0,0 +1,54 @@
+﻿using System.Data.SqlClient;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace IncludeSpec.Test.Utility
+{
+  public class TestDatabase
+  {
+    public string ConnectionString { get; private set; }
+
+    private TestDatabase(string connectionString)
+    {
+      ConnectionString = connectionString;
+    }
+
+    public static async Task<TestDatabase> Create(byte[] database)
+    {
+      var fileName = Path.GetTempFileName() + ".mdf";
+      File.WriteAllBytes(fileName, database);
+      var connectionBuilder = new SqlConnectionStringBuilder
+      {
+        DataSource = @"(LocalDB)\v12.0",
+        AttachDBFilename = fileName,
+        IntegratedSecurity = true
+      };
+
+      var connectionString = connectionBuilder.ConnectionString;
+
+      using (var connection = new SqlConnection(connectionString))
+      {
+        await connection.OpenAsync();
+      }
+
+      return new TestDatabase(connectionString);
+    }
+
+    public static async Task Delete(TestDatabase database)
+    {
+      SqlConnection.ClearAllPools();
+      var connectionBuilder = new SqlConnectionStringBuilder(database.ConnectionString);
+      var fileName = connectionBuilder.AttachDBFilename;
+
+      using (var conn = new SqlConnection(connectionBuilder.ConnectionString))
+      {
+        using (var cmd = new SqlCommand("drop database [" + fileName + "]", conn))
+        {
+          await conn.OpenAsync();
+          conn.ChangeDatabase("master");
+          await cmd.ExecuteNonQueryAsync();
+        }
+      }
+    }
+  }
+}
--- Sources/Test/IncludeSpec.EntityFramework.Test/IntegrationSourceTest.cs
+++ Sources/Test/IncludeSpec.EntityFramework.Test/IntegrationSourceTest.cs
@@ -37,5 +37,7 @@ namespace IncludeSpec.EntityFramework.Test
         }
       }
     }
+
+
   }
 }
